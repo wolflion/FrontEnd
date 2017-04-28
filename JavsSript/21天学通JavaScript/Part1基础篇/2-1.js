d668810731502dc0507ab2f36a3b713c24b47372
lionel Shen <successwolf@163.com>
2017-04-28 10:13:00 +0800

    js basic
    js basic

---
+++ JavsSript/21天学通JavaScript/Part1基础篇/2-1.js
@@ -0,0 +1,27 @@
+<script language ="javascript">
+<!-- 2-1
+var hello = "hello";
+var name = 'Peter';
+alert(hello + name);
+-->
+
+<!-- 2-2
+var str1="1,回车换行符\n";
+var str2="2,回车符\r";
+var str1="3,制表符\t,\n";
+var str1="4,\"双引号\"";
+var str = str1+str2+str3+str4;
+alert(str);
+-->
+
+<!-- 2-4
+var b1 = true;
+if(b1)
+	{document.write("变量b1的值为“\”真\"<br>");}
+var b2 = false;
+if(b2)
+	{document.write("变量b2的值为“\”真\"<br>");}
+
+
+-->
+</script>
---
+++ JavsSript/21天学通JavaScript/Part1基础篇/checkBrowser 2.js
@@ -0,0 +1,5 @@
+<script language = "JavaScript">
+document.write("名称：" + navigator.appName + "<br>");
+document.write("版本号：" + navigator.appVersion + "<br>");
+document.write("发行代号：" + navigator.appCodeName + "<br>");
+</script>
